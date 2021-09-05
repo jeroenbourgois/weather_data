24de4edd2729b13133f2c657e36fd4d6a574aa20
Jeroen Bourgois <jeroenbourgois@gmail.com>
2021-09-05 10:11:45 +0200

    chore: bump versions

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WeatherData.MixProject do
   def project do
     [
       app: :weather_data,
-      version: "0.1.0",
+      version: "2021.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
