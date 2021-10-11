3d87d35fad874c88f2340d22e546a0c4ace9c394
Jeroen Bourgois <jeroenbourgois@gmail.com>
2021-10-11 17:34:11 +0200

    feat: log date

--- config/prod.exs
+++ config/prod.exs
@@ -7,6 +7,6 @@ config :logger,
 
 config :logger, :file,
   path: "/var/log/weather_data.log",
-  format: "$time $metadata[$level] $message\n",
+  format: "$date $time $metadata[$level] $message\n",
   metadata: [:user_id],
   level: :debug
