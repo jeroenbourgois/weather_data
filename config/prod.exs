29255f2d3a37979fe9f962c695e24f8391ddd66c
Jeroen Bourgois <jeroenbourgois@gmail.com>
2021-10-11 18:05:53 +0200

    fix: logger config

--- config/prod.exs
+++ config/prod.exs
@@ -3,10 +3,11 @@ use Mix.Config
 config :weather_data, port: 80
 
 config :logger,
-  backends: [:console, {LoggerFileBackend, :file}]
+  backends: [:console, {LoggerFileBackend, :file}],
+  level: :info,
+  format:
+    "$date MIX_ENV=prod sudo /opt/app/weather_data/_build/prod/rel/weather_data/bin/weather_data start$time $metadata[$level] $message\n",
+  metadata: [:user_id]
 
 config :logger, :file,
   path: "/var/log/weather_data.log",
-  format: "$date $time $metadata[$level] $message\n",
-  metadata: [:user_id],
-  level: :debug
